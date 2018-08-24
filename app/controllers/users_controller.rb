90f56223830f923604f0d83d1814744ecf0cb5be
Shea Kusiak <shkusiak@gmail.com>
2018-08-24 17:08:16 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-
     if params[:username] == "" || params[:password] == "" || params[:email] == ""
       redirect '/signup'
     else
@@ -36,7 +35,7 @@ class UsersController < ApplicationController
   post '/login' do
     @user = User.find_by(username: params[:username])
     if !!@user && !!@user.authenticate(params[:password])
-
+      binding.pry
       session[:user_id] = @user.id
       redirect to '/tweets'
     else
